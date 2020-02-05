e59d84c2dbbed4f635f6f8e7d91b2a1c9a3b163b
Alex Romo III <alexromo3rd@gmail.com>
2020-02-04 20:47:26 -0800

    Complete The Basics exercises

---
+++ Basics_exercises/3.rb
@@ -0,0 +1,7 @@
+movies_list = {
+  :Saving_Private_Ryan => 1990,
+  :Spice_Girls => 1997,
+  :American_Pie => 1999
+}
+
+movies_list.each_value {|value| puts value}
---
+++ Basics_exercises/4.rb
@@ -0,0 +1,5 @@
+dates_list = [1990, 1997, 1999]
+
+dates_list.each do |date|
+  puts date
+end
